33a341233ff80467eaf20f98ec7b09f619dbf182
root-344 <michimasa8@gmail.com>
2021-07-29 17:09:12 +0900

    item, magic, skill画面作成

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -3,14 +3,41 @@ $(function() {
     $('.main').fadeOut(0);
     $('#modal1').fadeIn(0);
   });
-  $('.back-btn').click(function() {
+  $('.main-back-btn').click(function() {
     $('#modal1').fadeOut(0);
-    $('#modal2').fadeOut(0);
     $('.main').fadeIn(0);
+  });
+  $('.back-btn').click(function() {
+    $('#modal2').fadeOut(0);
+    $('#modal3').fadeOut(0);
+    $('#modal4').fadeOut(0);
+    $('#modal5').fadeOut(0);
+    // $('#modal6').fadeOut(0);
+    // $('#modal7').fadeOut(0);
+    $('#modal1').fadeIn(0);
   });
   $('.a').click(function() {
     $('#modal1').fadeOut(0);
     $('#modal2').fadeIn(0);
   });
-
+  $('.b').click(function() {
+    $('#modal1').fadeOut(0);
+    $('#modal3').fadeIn(0);
+  });
+  $('.c').click(function() {
+    $('#modal1').fadeOut(0);
+    $('#modal4').fadeIn(0);
+  });
+  $('.d').click(function() {
+    $('#modal1').fadeOut(0);
+    $('#modal5').fadeIn(0);
+  });
+  // $('.e').click(function() {
+  //   $('#modal1').fadeOut(0);
+  //   $('#modal6').fadeIn(0);
+  // });
+  // $('.f').click(function() {
+  //   $('#modal1').fadeOut(0);
+  //   $('#modal7').fadeIn(0);
+  // });
 });
